694f20f3e292d1c2b72c1656a89c46b65bbcce11
박태건 <parktaegeon@bagtaegeon-ui-MacBookAir.local>
2022-03-01 21:31:15 +0900

    creating Queries with Args and Subscription

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,34 @@
-const nicolas = {
-    name: 'nicolas',
-    age: 18,
-    gender: 'female'
-}
-const resolvers = {
-    Query: {
-      person: function() {
-          return nicolas;
-      }
+const people = [
+    {
+        id: 1,
+        name: 'nicolas2',
+        age: 18,
+        gender: 'female'
+    },
+    {
+        id: 2,
+        name:'nicolas2',
+        age: 18,
+        gender: 'female'
+    },
+    {
+        id: 3,
+        name: 'nicolas3',
+        age: 18,
+        gender: 'female'
     }
-  };
-  
-  export default resolvers;
+]
+
+export const getById = id => {
+    const filteredPeople = people.filter(person => person.id === id);
+    return filteredPeople[0];
+};
+
+const resolvers = {
+  Query: {
+    people: () => people,
+    person: (_, { id }) => getById(id)
+  }
+};
+
+export default resolvers;
